f13b4b2e12bb904b02d50e219f599ff1e024058d
akalend <akalend@mail.ru>
2011-01-10 12:45:01 +0300

    new file:   conf/app.conf 	new file:   conf/host.conf.php 	new file:   conf/localhost.nginx.conf 	new file:   conf/tfn24.nginx.conf 	new file:   conf/tpl.app.conf 	new file:   conf/tpl.nginx.conf

---
+++ conf/host.conf.php
@@ -0,0 +1,17 @@
+<?php
+$host_conf   = array (
+    'localhost' => array(
+        'static' => '/Users/akalend/projects/quickly/static/',
+        'root' => ' /Users/akalend/projects/quickly/www',
+        'news' => '/Users/akalend/projects/quickly/static/',
+        'upload' => '/Users/akalend/projects/quickly/tmp',        
+        
+    ),
+    'tfn24' => array (
+        'root' => '/home/www/tfn24/www/',
+        'news' => '/home/www/tfn24/static/news',
+        'static' => ' /home/www/tfn24/static/',
+        'upload' => '/tmp',
+        
+    ),
+);
